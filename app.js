a45926a6b3abb2ff128a6cfa1df7801a895b1742
jordansdj <jordansdjtecinfo@gmail.com>
2020-08-02 02:15:03 -0300

    criação da classe despesa e instância do objeto

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//abstração de uma despesa
+class Despesa{
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia 
+        this.tipo = tipo 
+        this.descricao = descricao
+        this.valor = valor 
+    }
+}
+
 //principal função responsavel por cadastrar as despesas
 function cadastrarDespesa(){
     //recuperação dos elementos da página de cadastro
@@ -8,5 +20,15 @@ function cadastrarDespesa(){
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    console.log(ano.value)
+    //instância do objeto "despesa"
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value, 
+        descricao.value, 
+        valor.value
+    )
+
+    console.log(despesa)
 }
