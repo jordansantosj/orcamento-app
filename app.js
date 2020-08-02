4b8863cc9aae4577b3ff1f0f4f8f82291869bcea
jordansdj <jordansdjtecinfo@gmail.com>
2020-08-02 02:06:07 -0300

    Recuperação de valores por DOM

---
+++ app.js
@@ -0,0 +1,12 @@
+//principal função responsavel por cadastrar as despesas
+function cadastrarDespesa(){
+    //recuperação dos elementos da página de cadastro
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    console.log(ano.value)
+}
